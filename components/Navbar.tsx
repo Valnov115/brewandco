9ff248bf1a17efc3115d11c14f5cafa7f7cdd113
Peter Besze <besze76@gmail.com>
2026-05-04 14:47:44 +0200

    refactor: extract shared CupIcon and NAV_LINKS to avoid duplication

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,25 +3,8 @@
 import { useState } from "react";
 import Link from "next/link";
 import ReservationModal from "@/components/ReservationModal";
-
-const NAV_LINKS = [
-  { label: "Home", href: "/" },
-  { label: "Menu", href: "/menu" },
-  { label: "About", href: "/about" },
-];
-
-const CupIcon = () => (
-  <svg width="18" height="18" viewBox="0 0 24 24" fill="none" aria-hidden="true">
-    <path
-      d="M18 8h1a4 4 0 0 1 0 8h-1M2 8h16v9a4 4 0 0 1-4 4H6a4 4 0 0 1-4-4V8z"
-      stroke="white"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <path d="M6 1v3M10 1v3M14 1v3" stroke="white" strokeWidth="2" strokeLinecap="round" />
-  </svg>
-);
+import { CupIcon } from "@/components/icons/CupIcon";
+import { NAV_LINKS } from "@/components/constants/navigation";
 
 export default function Navbar() {
   const [modalOpen, setModalOpen] = useState(false);
---
+++ components/constants/navigation.ts
@@ -0,0 +1,5 @@
+export const NAV_LINKS = [
+  { label: "Home", href: "/" },
+  { label: "Menu", href: "/menu" },
+  { label: "About", href: "/about" },
+];
